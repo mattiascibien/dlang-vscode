5af1bd8343bd7b1a0c6906e0dbb990f3b0e2999b
LaurentTreguier <laurent@treguier.org>
2016-03-25 22:33:28 +0100

    Remove logs that were not supposed to be here

--- src/dscanner.ts
+++ src/dscanner.ts
@@ -11,21 +11,15 @@ export default class Dscanner {
         let output = '';
         let dscanner = cp.spawn(Dscanner.path + 'dscanner', ['--report']);
 
-        console.log(Dscanner.path + 'dscanner');
-
         dscanner.stdout.on('data', (data) => {
-            console.log('data', data.toString());
             output += data;
         });
 
         dscanner.on('exit', () => {
-            console.log('exit');
-            console.log(output);
+            // TODO : create diagnostics when dscanner accepts code from stdin
         });
 
         dscanner.stdin.write(document.getText());
         dscanner.stdin.end();
-
-        console.log('cons');
     }
 }
