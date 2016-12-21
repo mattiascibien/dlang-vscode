5f27d8ccf13e1a7825f7a6ddd7f6003840fd0367
LaurentTreguier <laurent@treguier.org>
2016-12-21 20:59:45 +0100

    Move typings to package.json

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/escape-string-regexp/index.d.ts" />
-/// <reference path="globals/tmp/index.d.ts" />
-/// <reference path="globals/typescript/index.d.ts" />
-/// <reference path="globals/xml2js/index.d.ts" />
