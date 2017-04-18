edec455ed85ece29b23165881ac8ce5174974f40
Laurent Treguier <laurent@treguier.org>
2017-04-18 18:06:31 +0200

    Move problem matchers to package.json

--- src/tasks.ts
+++ src/tasks.ts
@@ -205,36 +205,12 @@ const defaultTasks = {
         {
             'taskName': 'build',
             'isBuildCommand': true,
-            'problemMatcher': {
-                'fileLocation': [
-                    'relative',
-                    '${workspaceRoot}'
-                ],
-                'pattern': {
-                    'regexp': '^(.+\\.di?)[\\D](\\d+)(,|:)?(\\d+)?\\S+\\s+([Ee]rror|[Ww]arning):\\s+(.+)$',
-                    'file': 1,
-                    'line': 2,
-                    'column': 4,
-                    'severity': 5,
-                    'message': 6
-                }
-            },
+            'problemMatcher': '$dub-build'
         },
         {
             'taskName': 'test',
             'isTestCommand': true,
-            'problemMatcher': {
-                'fileLocation': [
-                    'relative',
-                    '${workspaceRoot}'
-                ],
-                'pattern': {
-                    'regexp': '^.+@(.+\\.di?)\\((\\d+)\\S+\\s+(.+)$',
-                    'file': 1,
-                    'line': 2,
-                    'message': 3
-                }
-            }
+            'problemMatcher': '$dub-test'
         },
         {
             'taskName': 'run'
